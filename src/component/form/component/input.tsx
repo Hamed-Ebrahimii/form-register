3be512464c41b9379bc2fe7e8fa108fca114962d
Trainee1 <trainee1@arvandfanpars.com>
2024-04-28 09:45:18 +0330

    change input file

--- src/component/form/component/input.tsx
+++ src/component/form/component/input.tsx
@@ -1,7 +1,8 @@
-import {  MutationFunction } from "@tanstack/react-query";
+import { Avatar } from "@mui/material";
+import { MutationFunction, useMutation } from "@tanstack/react-query";
+import axios, { AxiosResponse } from "axios";
 
-
-import { DetailedHTMLProps } from "react";
+import { DetailedHTMLProps, useState } from "react";
 interface Iprops
   extends DetailedHTMLProps<
     React.InputHTMLAttributes<HTMLInputElement>,
@@ -10,21 +11,45 @@ interface Iprops
   lable: string;
   htmlFor: string;
   requier?: boolean;
-  error? : string,
-  accept? : 'image' | 'video' | 'music' | 'pdf'  | 'all',
-  uploadWidthChange? : boolean,
-  fnUpload? : (file : File) => MutationFunction<unknown, File>
-  isPending? : boolean
+  error?: string;
+  accept?: "image" | "video" | "music" | "pdf" | "all";
+  uploadWidthChange?: boolean;
+  fnUpload?: (file: File) => MutationFunction<unknown, File>;
+  isPending?: boolean;
+  numberFile? : number,
+  setError?  : (error : string) => void
 }
 const acceptType = {
-  image : 'image/*',
-  video : 'video/mp4,video/x-m4v,video/*',
-  music : 'audio/mp3',
-  pdf : 'application/pdf',
-  all : ''
-}
+  image: "image/*",
+  video: "video/mp4,video/x-m4v,video/*",
+  music: "audio/mp3",
+  pdf: "application/pdf",
+  all: "",
+};
 const Input = (props: Iprops) => {
+  const [file] = useState<string[]>([]);
+  const { mutate, isPending } = useMutation({
+    mutationFn: (file: FormData) =>
+      axios<AxiosResponse>("http://localhost:3000/student", {
+        method: "post",
+        data: file,
+      }),
+  });
+  function isImage(filename: File) {
+    const type = filename.type;
+    console.log(type);
 
+    switch (type.toLowerCase()) {
+      case "image/jpg":
+      case "image/gif":
+      case "image/bmp":
+      case "image/png":
+      case "image/jpeg":
+        //etc
+        return true;
+    }
+    return false;
+  }
   return (
     <div className="w-full flex flex-col items-start justify-center self-start gap-2 relative">
       <label
@@ -34,36 +59,62 @@ const Input = (props: Iprops) => {
         {props.lable}
         {props.requier && <span className="text-red-400 font-bold"> *</span>}
       </label>
-      {
-        props.type === "file" ?
+      {props.type === "file" ? (
         <>
-        <input
-        type="file"
-        onChange={(e)=>{
-          if(e.target.files && props.uploadWidthChange){
-           props.onChange && props.onChange(e)
-              
-             
-          }
-        }}
-        accept={acceptType[props.accept || 'all']}
-        className={`border w-full bg-gray-100  rounded-full py-1  outline-none placeholder:text-gray-400 text-gray-500 placeholder:text-xs placeholder:font-semibold file:mr-1 file:rounded-md file:border-0 file:ml-4  file:bg-blue-500 file:py-1 file:px-4 file:text-sm file:font-semibold file:text-white hover:file:bg-primary-700 ${props.type=== 'file' ? 'px-1' : 'px-5'} ${props.error ? 'border-red-400' : 'border-gray-200'}`}
-      />
-      
+          <input
+            type="file"
+            multiple={props.multiple}
+            onChange={(e) => {
+              if(props.numberFile && e.target.files?.length || 0  >= props.numberFile!){
+                  props.setError &&  props?.setError(`تعداد فایل های انتخاب شده بیشتر از ${props.numberFile} هست`)
+                  return
+              }
+              props.onChange && props.onChange(e);
+              if (
+                e.target.files &&
+                props.uploadWidthChange &&
+                isImage(e.target.files[0])
+              ) {
+                const formdata = new FormData();
+                const files = Array.from(e.target.files);
+                files.forEach((item) => {
+                  formdata.append(item.name, item);
+                });
+                for (let i = 0; i < files.length; i++) {
+                  file.push(URL.createObjectURL(files[i]));
+                }
+                mutate(formdata);
+                
+              }
+            }}
+            accept={acceptType[props.accept || "all"]}
+            className={`border w-full bg-gray-100  rounded-full py-1  outline-none placeholder:text-gray-400 text-gray-500 placeholder:text-xs placeholder:font-semibold file:mr-1 file:rounded-md file:border-0 file:ml-4  file:bg-blue-500 file:py-1 file:px-4 file:text-sm file:font-semibold file:text-white hover:file:bg-primary-700 ${
+              props.type === "file" ? "px-1" : "px-5"
+            } ${props.error ? "border-red-400" : "border-gray-200"}`}
+          />
         </>
-      :
-      <input
-        
-        {...props}
-        className={`border w-full bg-gray-100  rounded-full py-1  outline-none placeholder:text-gray-400 text-gray-500 placeholder:text-xs placeholder:font-semibold file:mr-1 file:rounded-md file:border-0 file:ml-4  file:bg-blue-500 file:py-1 file:px-4 file:text-sm file:font-semibold file:text-white hover:file:bg-primary-700 ${props.type=== 'file' ? 'px-1' : 'px-5'} ${props.error ? 'border-red-400' : 'border-gray-200'}`}
-      />
-      }
-      {
-        props.isPending && <progress className="progress w-56"></progress>
-      }
-      {
-        props.error && <p className="text-xs text-red-400 font-medium">{props.error}</p>
-      }
+      ) : (
+        <input
+          {...props}
+          className={`border w-full bg-gray-100  rounded-full py-1  outline-none placeholder:text-gray-400 text-gray-500 placeholder:text-xs placeholder:font-semibold file:mr-1 file:rounded-md file:border-0 file:ml-4  file:bg-blue-500 file:py-1 file:px-4 file:text-sm file:font-semibold file:text-white hover:file:bg-primary-700 ${
+            props.type === "file" ? "px-1" : "px-5"
+          } ${props.error ? "border-red-400" : "border-gray-200"}`}
+        />
+      )}
+      {isPending && <progress className="progress w-56"></progress>}
+      {props.error && (
+        <p className="text-xs text-red-400 font-medium">{props.error}</p>
+      )}
+      <div className="w-full flex items-center gap-3">
+        {props.type === "file" &&
+          file.map((item) => {
+            return (
+              <Avatar key={item} variant="rounded" className="cursor-pointer">
+                <img src={item} alt="" />
+              </Avatar>
+            );
+          })}
+      </div>
     </div>
   );
 };
--- src/component/formEducation/index.tsx
+++ src/component/formEducation/index.tsx
@@ -7,29 +7,25 @@ import {
   EducationFormType,
 } from "../../validation/registerFormValidation";
 import { parsNumberToString } from "../../utils/parsNumberToString";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ISearch } from "../dropDown/type";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getAllEducation } from "../../api/getAllEducation";
 import { setStudent, student } from "../../model/student";
-
-import axios from "axios";
 const FormEducation = () => {
   const {
     control,
     formState: { errors },
-    handleSubmit
-    , setValue
+    handleSubmit,
+    setValue,
+    setError
   } = useForm<EducationFormType>({
     mode: "all",
     resolver: zodResolver(EducationForm),
   });
   const onSubmit = (data: EducationFormType) => {
-    const newStudent = student
-    setStudent({...newStudent , ...data})
-    console.log(student);
-   
-
+    const newStudent = student;
+    setStudent({ ...newStudent, ...data });
   };
   const [convertNumberToString, setConvertNumberToString] = useState("");
   const [search, setSearch] = useState<ISearch>({
@@ -43,46 +39,42 @@ const FormEducation = () => {
     militaryService: "",
   });
 
-  const {data} = useQuery({
-    queryKey : [search.education],
-    queryFn : ()=> getAllEducation(search.education)
-  })
-  const {mutate , isPending} = useMutation({
-    mutationFn : (file : FormData) => axios('http://localhost:3000/student' , {
-      method : 'post',
-      data : file
-    })
-  })
-  
+  const { data } = useQuery({
+    queryKey: [search.education],
+    queryFn: () => getAllEducation(search.education),
+  });
+  const setErrorDegreePhoto = (error : string) =>{
+    setError('degreePhoto' , {message : error})
+  }
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className=" w-full rounded-xl px-8 py-12  shadow-sm "
+      className="w-full rounded-xl px-8 py-12  shadow-sm"
     >
       <section className="w-full grid grid-cols-4 gap-5 mt-6">
         <div className="flex items-end justify-between gap-4">
-        <Controller
-          name="degreeOfEducation"
-          control={control}
-          render={({ field }) => (
-            <DropDown
-              {...field}
-              checkBox
-              multiple
-              search={true}
-              error={errors.degreeOfEducation?.message}
-              fnData={() => {}}
-              lable="آخرین مدرک تحصیلی"
-              requier
-              name="education"
-              data={data?.data.map(item => item.name)}
-              setSearchValue={setSearch}
-              searchData={search}
-              placeholder="انتخاب کنید"
-              selectAll
-            />
-          )}
-        />
+          <Controller
+            name="degreeOfEducation"
+            control={control}
+            render={({ field }) => (
+              <DropDown
+                {...field}
+                checkBox
+                multiple
+                search={true}
+                error={errors.degreeOfEducation?.message}
+                fnData={() => {}}
+                lable="آخرین مدرک تحصیلی"
+                requier
+                name="education"
+                data={data?.data.map((item) => item.name)}
+                setSearchValue={setSearch}
+                searchData={search}
+                placeholder="انتخاب کنید"
+                selectAll
+              />
+            )}
+          />
         </div>
         <Controller
           name="average"
@@ -92,7 +84,7 @@ const FormEducation = () => {
               onChange={(e) => {
                 field.onChange(+e.target.value);
                 setConvertNumberToString(parsNumberToString(+e.target.value));
-                setValue("averageString" , convertNumberToString)
+                setValue("averageString", convertNumberToString);
               }}
               error={errors.average?.message}
               htmlFor=""
@@ -158,15 +150,11 @@ const FormEducation = () => {
         <Controller
           name="degreePhoto"
           control={control}
-          render={({ field  , fieldState}) => (
+          render={({ field }) => (
             <Input
               onChange={(e) => {
                 const file = Array.from(e.target.files || []);
                 field.onChange(file[0]);
-                const formdata = new FormData()
-                formdata.append(file[0].name , file[0])
-                console.log(fieldState.error?.message);
-                fieldState?.error?.message !=='' && mutate(formdata)
               }}
               error={errors.degreePhoto?.message}
               lable="فایل اسکن شده مدرک تحصیلی"
@@ -175,8 +163,10 @@ const FormEducation = () => {
               type="file"
               accept="image"
               uploadWidthChange
-              isPending={isPending}
+              multiple
               requier
+              numberFile={3}
+              setError={setErrorDegreePhoto}
             />
           )}
         />
